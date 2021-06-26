cda326ac8bd80fd8112cf162e4a7ece3c92cabb5
Sergio Torres <sergio.lo.torres@gmail.com>
2021-06-25 20:19:51 -0500

    pwa downloading added

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,4 @@
+
 const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
@@ -68,3 +69,4 @@ self.addEventListener('activate', function (e) {
       })
     );
   });
+
